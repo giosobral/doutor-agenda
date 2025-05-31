880ec204ae6c7ce5c16d4f08bd6b6925cb48c573
Jhowman037 <giovanesobral10@hotmail.com>
2025-05-31 20:56:42 -0300

    feat: implement session management and redirection in authentication and dashboard pages

---
+++ src/app/dashboard/components/sign-out-button.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ArrowRightFromLine } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+
+const SignOutButton = () => {
+  const router = useRouter();
+  return (
+    <Button
+      onClick={() =>
+        authClient.signOut({
+          fetchOptions: {
+            onSuccess: () => {
+              router.push("/authentication");
+            },
+          },
+        })
+      }
+    >
+      Sair
+      <ArrowRightFromLine />
+    </Button>
+  );
+};
+
+export default SignOutButton;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,25 @@
-const DashboardPage = () => {
-  return <h1> Tudo OK! </h1>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  return (
+    <div>
+      <h1>Bem-Vindo {session?.user?.name}</h1>
+      <SignOutButton />
+    </div>
+  );
 };
 
 export default DashboardPage;
