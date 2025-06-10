4f53fcfbb39511e6692bd5228b17ffb8e99ba17b
Jhowman037 <giovanesobral10@hotmail.com>
2025-06-10 19:56:08 -0300

    chore: ensure user redirected to /dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
-import { auth } from "@/lib/auth";
-
-export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  if (!session?.user) {
-    redirect("/authentication");
-  }
-  if (session.user.plan === "null") {
-    redirect("/new-subscription");
-  }
-
-  return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1>Hello World</h1>
-      <Button>+ Agendamento</Button>
-    </div>
-  );
+export default function Home() {
+  redirect("/dashboard");
 }
